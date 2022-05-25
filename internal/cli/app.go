47d372d392b386d09554777a00c5e4efdf9ae386
Peter Wagner <1559510+thepwagner@users.noreply.github.com>
2022-05-25 12:28:59 -0400

    HideHelpCommand

--- internal/cli/app.go
+++ internal/cli/app.go
@@ -6,12 +6,11 @@ import (
 )
 
 func NewApp(log logr.Logger) *cli.App {
-	// TODO: can we remove the "commands" abstraction?
-	// There is only one command
 	return &cli.App{
-		Name:   "hansel",
-		Usage:  "create empty packages as breadcrumbs for use when auditing container contents",
-		Flags:  GenerateFlags,
-		Action: Generate(log),
+		Name:            "hansel",
+		Usage:           "create empty packages as breadcrumbs for use when auditing container contents",
+		Flags:           GenerateFlags,
+		HideHelpCommand: true,
+		Action:          Generate(log),
 	}
 }
